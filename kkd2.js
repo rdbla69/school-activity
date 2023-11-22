aefb5fc0c706f96e1bb119941ca5967e561fda95
rdbla69 <109017728+rdbla69@users.noreply.github.com>
2023-11-23 01:22:02 +0800

    Update kkd2.js

--- kkd2.js
+++ kkd2.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
       createClickEffect(e.clientX, e.clientY);
     });
   });
-
   function createClickEffect(x, y) {
     var clickEffect = document.getElementById('click-effect');
     clickEffect.style.left = x - 10 + 'px'; // Adjust for half of the border width
@@ -14,3 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
       clickEffect.style.transform = 'scale(0)';
     }, 200);
   }
+  document.addEventListener('DOMContentLoaded', function() {
+    var branding = document.getElementById('branding');
+
+    branding.animate([{ opacity: 0 }, { opacity: 1 }], {
+        duration: 7000,
+        easing: 'linear', // Use 'linear' for a smooth, constant speed
+        iterations: Infinity
+    });
+  });
+  window.onscroll = function() {
+    var header = document.getElementById('sticky-header');
+    if (window.scrollY > 50 ){
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+};
